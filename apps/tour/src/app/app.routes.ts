8c0b4232a2471810cca575e07bd53beed7ce8673
Juan Ruiz <rmjuanruiz@gmail.com>
2023-09-21 12:11:08 -0500

    Initial Setup and create visitor library

--- apps/tour/src/app/app.routes.ts
+++ apps/tour/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'tour',
+    loadChildren: () =>
+      import('@rm-company/visitor').then((m) => m.VisitorModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'tour',
+  },
+];
