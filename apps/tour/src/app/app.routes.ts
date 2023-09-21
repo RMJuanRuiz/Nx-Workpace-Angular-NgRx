76db43c59bead302c120f345641aa3691a42ccbf
Juan Ruiz <rmjuanruiz@gmail.com>
2023-09-21 12:16:11 -0500

    Add Admin Library

--- apps/tour/src/app/app.routes.ts
+++ apps/tour/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'admin',
+    loadChildren: () => import('@rm-company/admin').then((m) => m.AdminModule),
+  },
+  {
     path: 'tour',
     loadChildren: () =>
       import('@rm-company/visitor').then((m) => m.VisitorModule),
